54cabe077b76c7188744afa5a1ec49c7e5b5c611
MukhamedovK <kamoliddin2619@gmail.com>
2025-04-12 18:07:17 +0500

    renamed swagger title

--- src/config/swaggerConfig.js
+++ src/config/swaggerConfig.js
@@ -8,7 +8,7 @@ const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "API Documentation Zaporka project",
+      title: "API Documentation Black Pheonix project",
       version: "1.0.0",
       description: "Documentation for the backend APIs",
     },
